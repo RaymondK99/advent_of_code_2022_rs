da6064b8d3acc858b3057e0fd46bef4c67c29778
Raymond Kalander <raymond.kalander@getswish.se>
2022-12-06 12:12:02 +0100

    prep until day 25

---
+++ src/util/day_21.rs
@@ -0,0 +1,55 @@
+use super::Part;
+
+pub fn solve(input : String, part: Part) -> String {
+
+    match part {
+        Part::Part1 => part1(input),
+        Part::Part2 => part2(input)
+    }
+}
+
+fn part1(_input : String) -> String {
+    "1".to_string()
+}
+
+fn part2(_input : String) -> String {
+    "1".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+    use util::Part::{Part1, Part2};
+
+
+    #[test]
+    fn test1() {
+
+        let input = "";
+
+        assert_eq!("1", solve(input.to_string(), Part1));
+    }
+
+    #[test]
+    fn test_part1() {
+        let input = include_str!("../../input/input_21.txt");
+
+        assert_eq!("1", solve(input.to_string(), Part1));
+    }
+
+    #[test]
+    fn test2() {
+
+        let input = "";
+
+        assert_eq!("1", solve(input.to_string(), Part2));
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = include_str!("../../input/input_21.txt");
+
+        assert_eq!("1", solve(input.to_string(), Part2));
+    }
+}
